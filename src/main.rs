d27e3a293febc39c009a3b77f543d0a1a837fb1f
Tom Mendy <tom.mendy@epitech.eu>
2024-12-20 11:58:24 +0100

    [ADD] text

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-use std::ops::Index;
-use std::time::Duration;
-
+use rand::Rng;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
 use sdl2::rect::{Point, Rect};
 use sdl2::render::Canvas;
-use sdl2::ttf::{self, Font};
+use sdl2::render::{Texture, TextureCreator, TextureQuery};
+use sdl2::surface::Surface;
+use sdl2::ttf::{self, Font, Sdl2TtfContext};
 use sdl2::video::Window;
-
-use rand::Rng;
+use sdl2::video::WindowContext;
+use std::ops::Index;
+use std::time::Duration;
 
 fn draw_circle(canvas: &mut Canvas<Window>, center: Point, radius: i32) -> Result<(), String> {
     let mut x = radius;
@@ -140,14 +142,16 @@ fn handle_even(event_pump: &mut sdl2::EventPump, list_rect: &mut Vec<Rect>, game
     }
 }
 
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 enum GameStatus {
     Exit,
     Pause,
     Running,
 }
 
+#[derive(Debug)]
 struct Game {
+    name: String,
     game_state: GameStatus,
     size_grid: i32,
     window_height: i32,
@@ -155,39 +159,66 @@ struct Game {
     unit_grid: i32,
 }
 
-fn main() -> Result<(), String> {
-    let sdl_context = sdl2::init()?;
-    let video_subsystem = sdl_context.video()?;
+impl Game {
+    fn new() -> Self {
+        Self {
+            name: "Rust Of Life".to_string(),
+            game_state: GameStatus::Running,
+            size_grid: 100,
+            window_height: 1000,
+            window_width: 1000,
+            unit_grid: 0,
+        }
+    }
+    fn calculate_unit_grid(&mut self) -> () {
+        self.unit_grid = (self.window_width / self.size_grid) as i32;
+    }
+}
 
-    let mut game_info: Game = Game {
-        game_state: GameStatus::Running,
-        size_grid: 100,
-        window_height: 1000,
-        window_width: 1000,
-        unit_grid: 0, // will be calculated later
-    };
-    game_info.unit_grid = (game_info.window_width / game_info.size_grid) as i32;
+fn init_canvas(
+    title: &str,
+    width: u32,
+    height: u32,
+    draw_color: Color,
+) -> Result<(sdl2::Sdl, Canvas<Window>), String> {
+    // Initialize SDL2
+    let sdl_context = sdl2::init().map_err(|e| e.to_string())?;
+    let video_subsystem = sdl_context.video().map_err(|e| e.to_string())?;
 
+    // Create a window
     let window = video_subsystem
-        .window(
-            "Rust Of Life",
-            game_info.window_width as u32,
-            game_info.window_height as u32,
-        )
+        .window(title, width, height)
         .position_centered()
         .build()
         .map_err(|e| e.to_string())?;
 
+    // Initialize the canvas
     let mut canvas = window
         .into_canvas()
+        .present_vsync()
         .accelerated()
         .build()
         .map_err(|e| e.to_string())?;
-    canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
-    //let texture_creator = canvas.texture_creator();
-    //let mut tex = texture_creator
-    //    .create_texture_target(None, 400, 400)
-    //    .map_err(|_| String::from("Unable to create texture."))?;
+    canvas.set_draw_color(draw_color);
+    canvas.clear();
+    canvas.present();
+
+    Ok((sdl_context, canvas))
+}
+
+fn main() -> Result<(), String> {
+    /////////////////////////////////////////////
+
+    let mut game_info: Game = Game::new();
+    game_info.calculate_unit_grid();
+
+    let (sdl_context, mut canvas) = init_canvas(
+        &game_info.name,
+        game_info.window_width as u32,
+        game_info.window_height as u32,
+        Color::RGB(0, 0, 0),
+    )?;
+    println!("game_info is {game_info:?}");
 
     //let timer = sdl_context.timer()?;
 
@@ -205,13 +236,39 @@ fn main() -> Result<(), String> {
     // The following demonstrates a type that implements Into<&[Point]>
     let borrowed_slice: &[Point] = &points_slice.iter().map(|&p| p).collect::<Vec<Point>>()[..];
 
-    // Initialize the TTF context
+    // Initialize TTF context
     let ttf_context = ttf::init().map_err(|e| e.to_string())?;
+
+    // Load font
+    let font = init_font("./Roboto-Medium.ttf", 128, &ttf_context)?;
+
+    // Render the text to a surface, then create a texture
+    let texture_creator = canvas.texture_creator();
+    let texture = generate_texture(
+        &font,
+        "Hello, World!",
+        Color::RGB(255, 255, 255),
+        &texture_creator,
+    )?;
+
+    // Query the texture for width and height
+    let TextureQuery { width, height, .. } = texture.query();
+
+    // Center the text on the screen
+    let target = Rect::new(
+        (800 - width as i32) / 2,
+        (600 - height as i32) / 2,
+        width,
+        height,
+    );
+
+    // Draw the texture to the canvas
 
     // Load the font using `from_file`
     //let font = Font::from_file(&ttf_context, "Roboto-Medium.ttf", 128).map_err(|e| e.to_string());
 
     let mut list_rect: Vec<Rect> = Vec::new();
+    canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
     while game_info.game_state != GameStatus::Exit {
         handle_even(&mut event_pump, &mut list_rect, &mut game_info);
 
@@ -239,6 +296,8 @@ fn main() -> Result<(), String> {
         canvas.draw_lines(borrowed_slice)?;
         canvas.fill_rects(&list_rect)?;
         canvas.set_draw_color(sdl2::pixels::Color::RGB(255, 255, 255));
+
+        canvas.copy(&texture, None, Some(target))?;
         //font.render("Hello Rust!", sdl2::pixels::Color::RGB(0, 0, 0))
         //    .map_err(|e| e.to_string())?;
         canvas.present();
@@ -246,4 +305,37 @@ fn main() -> Result<(), String> {
     }
 
     Ok(())
+}
+
+fn init_font<'a>(
+    font_path: &str,
+    font_size: u16,
+    ttf_context: &'a Sdl2TtfContext,
+) -> Result<Font<'a, 'a>, String> {
+    // Load the font
+    let font = ttf_context
+        .load_font(font_path, font_size)
+        .map_err(|e| e.to_string())?;
+
+    Ok(font)
+}
+
+fn generate_texture<'a>(
+    font: &Font,
+    text: &str,
+    color: Color,
+    texture_creator: &'a TextureCreator<WindowContext>,
+) -> Result<Texture<'a>, String> {
+    // Render the text to a surface
+    let surface: Surface = font
+        .render(text)
+        .blended(color)
+        .map_err(|e| e.to_string())?;
+
+    // Create a texture from the surface
+    let texture: Texture = texture_creator
+        .create_texture_from_surface(&surface)
+        .map_err(|e| e.to_string())?;
+
+    Ok(texture)
 }
