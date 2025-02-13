0f2e555d2b11371fea3765b8d0d8bc98e8aee7c3
Tom Mendy <tom.mendy@epitech.eu>
2025-02-13 13:40:27 +0100

    [UPD] update test script for coverage generation; add support for NixOS and install necessary tools

--- test.sh
+++ test.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
+# Install the necessary tools
+rustup component add llvm-tools-preview
+cargo install grcov
+
+rm -rf target/
+rm default*.profraw
+
 # Run the tests
-cargo test
+. /etc/os-release
+case "${ID}" in
+nixos)
+    nix develop --command bash -c 'RUSTFLAGS="-C instrument-coverage" cargo test --tests'
+    ;;
+*)
+    RUSTFLAGS="-C instrument-coverage" cargo test --tests
+    ;;
+esac
 
 # Generate the coverage report
 grcov . --binary-path ./target/debug/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore '/*' -o ./target/debug/coverage/
 
 # Open the coverage report in the browser
 xdg-open ./target/debug/coverage/index.html
+
+rm default*.profraw
